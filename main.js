44aaeb504b9fb26f0e1bc9e26fc9ac2c0e99f700
Duy Vu <duyv.thanh@gmail.com>
2018-10-24 11:34:19 -0600

    Adds inputs

--- main.js
+++ main.js
@@ -4,25 +4,24 @@ var upVote              =        document.querySelector('.up-vote')
 var downVote            =        document.querySelector('.down-vote')
 var deleteButton        =        document.querySelector('.delete-button')
 var bottomSection       =        document.querySelector('.bottom-section')
-var cardDisplay         =        document.querySelector('.card-display')
+// var cardDisplay         =        document.querySelector('.card-display')
 
-saveButton.addEventListener('click', appendCard)
-// searchButton.addEventListener('click',)
-// upVote.addEventListener('click',)
-// downVote.addEventListener('click',)
-// deleteButton.addEventListener('click',);
+saveButton.addEventListener('click', saveReturn)
 
-// function saveClick() {
-//   appendCard('title', 'text');
-// }
+function saveReturn() {
+  var titleInput = document.querySelector('.title-input')
+  var bodyInput = document.querySelector('.body-input')
+  appendCard(titleInput.value, bodyInput.value);
 
-function appendCard() {
+}
+
+function appendCard(title, body) {
   event.preventDefault();
   // console.log('Hey')
   var cardHtml =
     `<div class="card-display">
-    <h2 class="card-title">Lorem Ipsum</h2>
-    <p class="card-text">lorem</p>
+    <h2 class="card-title">${title}</h2>
+    <p class="card-text">${body}</p>
       <button class="up-vote">up</button>
       <button class="down-vote">down</button>
       <p class="quality-change">Quality: Swill</p>
